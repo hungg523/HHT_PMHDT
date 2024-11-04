df94efc58cd47512f2c4bc45bbf013bdf4868321
BHieeuss <tbh.11203@gmail.com>
2024-11-05 01:02:30 +0700

    .

--- src/NhaThuoc.Application/Request/Category/GetByNameCategory.cs
+++ src/NhaThuoc.Application/Request/Category/GetByNameCategory.cs
@@ -1,8 +1,9 @@
 ﻿using MediatR;
+using Entities = NhaThuoc.Domain.Entities;
 
 namespace NhaThuoc.Application.Request.Category
 {
-    public class GetByNameCategory : IRequest<Category>
+    public class GetByNameCategory : IRequest<Entities.Category>
     {
         public int? Id { get; set; }
     }
--- src/NhaThuoc.WebApi/Program.cs
+++ src/NhaThuoc.WebApi/Program.cs
@@ -1,17 +1,29 @@
 using NhaThuoc.Application.Extensions;
 using NhaThuoc.Data.Extensions;
 using NhaThuoc.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-//builder.Services.AddData(builder.Configuration);
 
+
+// Add services to the container.
 builder.Services.AddData(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddControllers();
+
+// C?u hình CORS ?? cho phép truy c?p t? http://127.0.0.1:5500
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy => policy.WithOrigins("http://127.0.0.1:5500")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,8 +33,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Áp d?ng CORS tr??c các middleware khác
+app.UseCors("AllowSpecificOrigin");
+
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
