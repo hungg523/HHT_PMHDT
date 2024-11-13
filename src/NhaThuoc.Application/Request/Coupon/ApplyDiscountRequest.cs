803113b6fde094f297435c4ce94ea948fd2247c7
Tới Văn <tovantoi2003@gmail.com>
2024-11-13 20:38:44 +0700

    add

---
+++ src/NhaThuoc.Application/Request/Coupon/ApplyDiscountRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using NhaThuoc.Share.Exceptions;
+using Entities = NhaThuoc.Domain.Entities;
+
+namespace NhaThuoc.Application.Request.Coupon
+{
+    public class ApplyDiscountRequest : IRequest<ApiResponse>
+    {
+        
+    }
+}
