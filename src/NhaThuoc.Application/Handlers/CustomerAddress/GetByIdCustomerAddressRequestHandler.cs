1a023a9e26b35d6f8d7f3f3a2d8f56e1ee9f6bba
Tới Văn <tovantoi2003@gmail.com>
2024-11-14 14:57:22 +0700

    update getbyid

--- src/NhaThuoc.Application/Handlers/CustomerAddress/GetByIdCustomerAddressRequestHandler.cs
+++ src/NhaThuoc.Application/Handlers/CustomerAddress/GetByIdCustomerAddressRequestHandler.cs
@@ -2,16 +2,17 @@
 using MediatR;
 using NhaThuoc.Application.Request.Customers.CustomerAddress;
 using NhaThuoc.Domain.Abtractions.IRepositories;
+using NhaThuoc.Share.DependencyInjection.Extensions;
 using Entities = NhaThuoc.Domain.Entities;
 
 namespace NhaThuoc.Application.Handlers.CustomerAddress
 {
-    public class GetByNameCustomerAddressRequestHandler : IRequestHandler<GetByIdCustomerAddressRequest, Entities.CustomerAddress>
+    public class GetByIdCustomerAddressRequestHandler : IRequestHandler<GetByIdCustomerAddressRequest, Entities.CustomerAddress>
     {
         private readonly ICustomerAddressRepository customerAddressRepository;
         private readonly IMapper mapper;
 
-        public GetByNameCustomerAddressRequestHandler(ICustomerAddressRepository customerAddressRepository, IMapper mapper)
+        public GetByIdCustomerAddressRequestHandler(ICustomerAddressRepository customerAddressRepository, IMapper mapper)
         {
             this.customerAddressRepository = customerAddressRepository;
             this.mapper = mapper;
@@ -19,6 +20,7 @@ namespace NhaThuoc.Application.Handlers.CustomerAddress
         public async Task<Domain.Entities.CustomerAddress> Handle(GetByIdCustomerAddressRequest request, CancellationToken cancellationToken)
         {
             var customeraddress = await customerAddressRepository.FindByIdAsync(request.Id);
+            if (customeraddress is null) customeraddress.ThrowNotFound();
             return mapper.Map<Domain.Entities.CustomerAddress>(customeraddress);
         }
     }
--- src/NhaThuoc.Application/Request/Category/GetByIdCategoryRequest.cs
+++ src/NhaThuoc.Application/Request/Category/GetByIdCategoryRequest.cs
@@ -3,7 +3,7 @@ using Entities = NhaThuoc.Domain.Entities;
 
 namespace NhaThuoc.Application.Request.Category
 {
-    public class GetByNameCategoryRequest : IRequest<Entities.Category>
+    public class GetByIdCategoryRequest : IRequest<Entities.Category>
     {
         public int? Id { get; set; }
     }
